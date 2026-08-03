c51ae51a1483608feefa9add80db369e2e8f72a0
Pedro da Costa Lopes <pedrolopes.programador013@gmail.com>
2026-08-03 12:57:53 -0300

    addd: adicao do id_user e user no model do subscription

--- Models/Subscription.cs
+++ Models/Subscription.cs
@@ -9,21 +9,32 @@ namespace StreamingSubscriptionTrackerAPI.Models
         [Column("id")]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public long Id { get; set; }
+        
         [Required]
         [Column("name", TypeName = "varchar(80)")]
         [MaxLength(80)]
         public string Name { get; set; }
+        
         [Required]
         [Column("price", TypeName = "decimal(10,2)")]
         public decimal Price { get; set; }
+        
         [Required]
         [Column("date_to_paid", TypeName = "date")]
         public DateOnly DateToPaid { get; set; }
+        
         [Required]
         [Column("id_category")]
         public long IdCategory { get; set; }
 
         [ForeignKey(nameof(IdCategory))]
         public SubscriptionCategory Category { get; set; }
+        
+        [Required]
+        [Column("id_user")]
+        public long IdUser { get; set; }
+
+        [ForeignKey(nameof(IdUser))]
+        public User User { get; set; }
     }
 }
