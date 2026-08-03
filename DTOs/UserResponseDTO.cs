47a8fcb3bf7006655b09f81f3a1925630e5f35d4
Pedro da Costa Lopes <pedrolopes.programador013@gmail.com>
2026-08-03 13:22:24 -0300

    addd: criacao do UserResponseDTO

--- DTOs/UserResponseDTO.cs
+++ DTOs/UserResponseDTO.cs
@@ -2,5 +2,10 @@
 {
     public class UserResponseDTO
     {
+        public long Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public bool Actived { get; set; }
+        public DateOnly CreatedAt { get; set; }
     }
 }
