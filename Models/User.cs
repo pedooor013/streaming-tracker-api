39974e4dd601607613f1aa1e38c83f854ffd3499
Pedro da Costa Lopes <pedrolopes.programador013@gmail.com>
2026-08-03 12:54:14 -0300

    addd: desenvolvimento do User no Model

---
+++ Models/User.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StreamingSubscriptionTrackerAPI.Models
+{
+    public class User
+    {
+        [Key]
+        [Column("id")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    }
+}
