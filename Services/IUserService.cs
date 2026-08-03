743147867a4bcc3515a7f630b5e4f69ec475c86e
Pedro da Costa Lopes <pedrolopes.programador013@gmail.com>
2026-08-03 13:27:46 -0300

    addd: criacao do IUserService

---
+++ Services/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using StreamingSubscriptionTrackerAPI.DTOs;
+namespace StreamingSubscriptionTrackerAPI.Services
+{
+    public interface IUserService
+    {
+        //GET
+        UserResponseDTO GetById(long id);
+
+        //POST
+        //PUT
+        //DELETE
+    }
+}
